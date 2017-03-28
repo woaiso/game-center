69f0720c0f43e21506211be5808c7ce3aa695b2f
woaiso <woaiso@woaiso.com>
2017-03-28 23:14:26 +0800

    add root page

---
+++ src/main/index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+class Main extends React.Components {
+  render() {
+    return (
+        <div>
+          Game Center
+        </div>
+    );
+  }
+}
+
+ReactDOM.render(<Main />, document.getElementById('root'));
