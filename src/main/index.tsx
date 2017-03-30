c6aab7de035dc613994c8c3fa52533da6cf7ad6d
woaiso <woaiso@woaiso.com>
2017-03-30 22:30:11 +0800

    add head

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -12,12 +12,23 @@ import * as ReactDOM from 'react-dom';
 import Header from './../components/header';
 import './respones';
 import 'normalize.css/normalize.css';
+import './main.less';
+const head_bg = require('./../images/head_bg.png');
+const head = require('./../images/head.png');
 
 class Main extends React.Component<any, any> {
   render() {
     return (
       <div>
         <Header title="我的游戏" />
+        <div className="game-content">
+          <div className="head-content" style={{ backgroundImage: 'url(' + head_bg + ')' }}>
+            <div className="head-inner">
+              <img src={head} className="user-head" />
+              <span className="user-text">侞果僾不篱</span>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
