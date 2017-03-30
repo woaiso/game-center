0c94043b93e2cbe169d2f161fd7d63d196bed60e
woaiso <woaiso@woaiso.com>
2017-03-30 23:43:18 +0800

    update

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -15,6 +15,35 @@ import 'normalize.css/normalize.css';
 import './main.less';
 const head_bg = require('./../images/head_bg.png');
 const head = require('./../images/head.png');
+const gameIcon = require('./../images/game_01.png');
+
+const games = [
+  {
+    icon: require('./../images/game_01.png'),
+    text: '我的猫呢-喵魂 设计 柠檬酱测试文本'
+  },
+  {
+    icon: require('./../images/game_02.png'),
+    text: '糖果苏打传奇'
+  },
+  {
+    icon: require('./../images/game_03.png'),
+    text: '冒险村物语'
+  },
+  {
+    icon: require('./../images/game_04.png'),
+    text: '地牢战争'
+  },
+]
+
+const GameItem = ({ game }) => (
+  <li className="item">
+    <div className="icon" style={{ backgroundImage: 'url(' + game.icon + ')' }} />
+    <span className="name">
+      {game.text}
+    </span>
+  </li>
+);
 
 class Main extends React.Component<any, any> {
   render() {
@@ -33,6 +62,11 @@ class Main extends React.Component<any, any> {
           <section className="g-item">
             <div className="title">
               <h5>最近在玩</h5>
+            </div>
+            <div className="game-list">
+              <ul>
+                {games.map((item, index) => <GameItem game={item} key={index} />)}
+              </ul>
             </div>
           </section>
         </div>
