f9aa9de0998cc342521155d05a2e76f6fef90936
woaiso <woaiso@woaiso.com>
2017-03-31 22:35:16 +0800

    edit

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -10,8 +10,10 @@ import * as ReactDOM from 'react-dom';
 // const {Button} = WeUI;
 
 import Header from './../components/header';
+import Button from './../components/button';
 import './respones';
-import 'normalize.css/normalize.css';
+import './../components/style/index.less';
+
 import './main.less';
 const head_bg = require('./../images/head_bg.png');
 const head = require('./../images/head.png');
@@ -60,7 +62,61 @@ const gamelist = [
       }
     ]
   }
+];
+
+const timelines = [
+  {
+    date: {
+      day: '08',
+      month: '12月'
+    },
+    card_title: '最近登录',
+    card_subtitle: '2016-10-11加入',
+    game_icon: require('./../images/game_02.png'),
+    game_title: '太空猫和原子猫头鹰子猫（策略类大作）',
+    game_badge: '策略类',
+    game_subtitle: '搜集小伙伴一起战斗吧！集小伙伴一意起XXXXXXXX'
+  },
+  {
+    date: {
+      day: '25',
+      month: '12月'
+    },
+    card_title: '下载游戏',
+    card_subtitle: '',
+    game_icon: require('./../images/game_05.png'),
+    game_title: '爆彩童话',
+    game_badge: '策略类',
+    game_subtitle: '搜集小伙伴一起战斗吧！'
+  }, {
+    year: '2015年'
+  },
+  {
+    date: {
+      day: '08',
+      month: '12月'
+    },
+    card_title: '最近登录',
+    card_subtitle: '2016-10-11加入',
+    game_icon: require('./../images/game_02.png'),
+    game_title: '太空猫和原子猫头鹰子猫（策略类大作）',
+    game_badge: '策略类',
+    game_subtitle: '搜集小伙伴一起战斗吧！集小伙伴一意起XXXXXXXX'
+  },
+  {
+    date: {
+      day: '25',
+      month: '12月'
+    },
+    card_title: '下载游戏',
+    card_subtitle: '',
+    game_icon: require('./../images/game_05.png'),
+    game_title: '爆彩童话',
+    game_badge: '策略类',
+    game_subtitle: '搜集小伙伴一起战斗吧！'
+  }
 ]
+
 
 const GameItem = ({ gameItem }) => (
   <li className="item">
@@ -101,64 +157,59 @@ export default class Main extends React.Component<any, any> {
           {gamelist.map((game, index) => <Game game={game} key={index} />)}
         </div>
         {/*历程*/}
-        <TimeLine />
+        <div className="timeline">
+          <div className="head">
+            <h5 className="title">历程</h5>
+            <div className="tabs">
+              <div className="tab current">全部</div>
+              <div className="tab">手游</div>
+              <div className="tab">H5</div>
+            </div>
+          </div>
+          {/*历程时间轴*/}
+          <ul className="t-content">
+            {timelines.map((timeline, index) => (timeline as any).year ? <Year year={(timeline as any).year} /> : <TimeLine timeline={timeline} key={index} />)}
+          </ul>
+        </div>
       </div>
     );
   }
 }
 
-const TimeLine = () => (
-  <div className="timeline">
-    <div className="head">
-      <h5 className="title">历程</h5>
-      <div className="tabs">
-        <div className="tab current">全部</div>
-        <div className="tab">手游</div>
-        <div className="tab">H5</div>
-      </div>
-    </div>
-    {/*历程时间轴*/}
-    <ul className="t-content">
-      <li>
-        <div className="t-item">
-          <span className="date">
-            <span className="day">08</span>
-            <span className="month">12月</span>
-          </span>
-          <div className="card">
-            <h5 className="title">最近登录 <span className="sub">2016-10-11加入</span> </h5>
-            <div className="game-item-h">
-              <div className="icon" style={{ backgroundImage: 'url(' + exampleIcon + ')' }} />
-              <div className="detail">
-                <h5 className="title">太空猫和原子猫头鹰子猫...</h5>
+const Year = ({ year }) => (
+  <li>
+    <div className="year">{year}</div>
+  </li>
+);
+
+const TimeLine = ({ timeline }) => (
+  <li>
+    <div className="t-item">
+      <span className="date">
+        <span className="day">{timeline.date.day}</span>
+        <span className="month">{timeline.date.month}</span>
+      </span>
+      <div className="card">
+        <h5 className="title">{timeline.card_title} <span className="sub">{timeline.card_subtitle}</span> </h5>
+        <div className="game-item-h">
+          <div className="icon" style={{ backgroundImage: 'url(' + timeline.game_icon + ')' }} />
+          <div>
+            <div className="detail">
+              <div>
+                <h5 className="title">{timeline.game_title}</h5>
                 <div>
-                  <button className="group-badge">策略类</button>
+                  <Button type="primary" outline={true} className="group-badge">{timeline.game_badge}</Button>
                 </div>
                 <div className="sub-title">
-                  搜集小伙伴一起战斗吧！集小伙伴一意起
+                  {timeline.game_subtitle}
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </li>
-    </ul>
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-  </div>
+      </div>
+    </div>
+  </li>
 );
 
 ReactDOM.render(<Main />, document.getElementById('root'));
