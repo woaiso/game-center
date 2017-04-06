284e4e4ca3e51694f766f5fe9d7c95eccec93547
woaiso <woaiso@woaiso.com>
2017-04-06 21:25:29 +0800

    add

---
+++ src/index/games.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import Header from './../components/header';
+import { Page, Content, Slider } from './container';
+import { Game } from './main';
+import { gamelist } from './data';
+/**
+ * 游戏界面
+ */
+
+export default class Games extends React.Component<any, any> {
+  render() {
+    return (
+      <Page>
+        <Header />
+        <Content>
+          <Slider className="game-slider" />
+          {/*中间内容区*/}
+          <Game game={gamelist[0]} />
+        </Content>
+      </Page>
+    );
+  }
+}
