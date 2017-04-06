daab486ec1c461277018ab4edd5b6c2b941191d1
woaiso <woaiso@woaiso.com>
2017-04-06 21:25:25 +0800

    add data

---
+++ src/index/data.ts
@@ -0,0 +1,98 @@
+export const gamelist = [
+  {
+    title: '最近在玩',
+    more: 'http://game.qq.com',
+    games: [
+      {
+        icon: require('./../images/game_01.png'),
+        text: '我的猫呢-喵魂 设计 柠檬酱测试文本'
+      },
+      {
+        icon: require('./../images/game_02.png'),
+        text: '糖果苏打传奇'
+      },
+      {
+        icon: require('./../images/game_03.png'),
+        text: '冒险村物语'
+      },
+      {
+        icon: require('./../images/game_04.png'),
+        text: '地牢战争'
+      }
+    ]
+  },
+  {
+    title: '最近下载',
+    games: [
+      {
+        icon: require('./../images/game_01.png'),
+        text: '我的猫呢-喵魂 设计 柠檬酱测试文本'
+      },
+      {
+        icon: require('./../images/game_02.png'),
+        text: '糖果苏打传奇'
+      },
+      {
+        icon: require('./../images/game_03.png'),
+        text: '冒险村物语'
+      },
+      {
+        icon: require('./../images/game_04.png'),
+        text: '地牢战争'
+      }
+    ]
+  }
+];
+
+export const timelines = [
+  {
+    date: {
+      day: '08',
+      month: '12月'
+    },
+    card_title: '最近登录',
+    card_subtitle: '2016-10-11加入',
+    game_icon: require('./../images/game_02.png'),
+    game_title: '太空猫和原子猫头鹰子猫（策略类大作）',
+    game_badge: '策略类',
+    game_subtitle: '搜集小伙伴一起战斗吧！集小伙伴一意起XXXXXXXX'
+  },
+  {
+    date: {
+      day: '25',
+      month: '12月'
+    },
+    card_title: '下载游戏',
+    card_subtitle: '',
+    game_icon: require('./../images/game_05.png'),
+    game_title: '爆彩童话',
+    game_badge: '策略类',
+    game_subtitle: '搜集小伙伴一起战斗吧！'
+  }, {
+    year: '2015年'
+  },
+  {
+    date: {
+      day: '08',
+      month: '12月'
+    },
+    card_title: '最近登录',
+    card_subtitle: '2016-10-11加入',
+    game_icon: require('./../images/game_02.png'),
+    game_title: '太空猫和原子猫头鹰子猫（策略类大作）',
+    game_badge: '策略类',
+    game_subtitle: '搜集小伙伴一起战斗吧！集小伙伴一意起XXXXXXXX'
+  },
+  {
+    date: {
+      day: '25',
+      month: '12月'
+    },
+    card_title: '下载游戏',
+    card_subtitle: '',
+    game_icon: require('./../images/game_05.png'),
+    game_title: '爆彩童话',
+    game_badge: '策略类',
+    game_subtitle: '搜集小伙伴一起战斗吧！'
+  }
+]
