625a4a0cc9abd45ac82fab6ea8b2b558958d6f38
woaiso <woaiso@woaiso.com>
2017-04-12 00:57:31 +0800

    add star

--- src/index/GameList.tsx
+++ src/index/GameList.tsx
@@ -15,12 +15,23 @@ const GameItem = () => (
       <div className="group-badge">
         <Button type="primary" outline={true}>策略类</Button>
       </div>
+      <div className="star">
+        <svg width="30" height="30" xmlns="http://www.w3.org/2000/svg" viewBox="0 12.705 512 486.59" style={{ "margin-right": "0px" }} >
+          <defs>
+            <linearGradient id="demo-onchange_grad5" x1="0%" y1="0%" x2="100%" y2="0%">
+              <stop offset="50%" stop-color="rgba(51,155,0,1)" />
+              <stop offset="0%" stop-color="white" />
+            </linearGradient>
+          </defs>
+          <polygon style={{ "fill": "url(#demo-onchange_grad5)", stroke: "black", "stroke-width": "2px" }} points="256.814,12.705 317.205,198.566 512.631,198.566 354.529,313.435 414.918,499.295 256.814,384.427 98.713,499.295 159.102,313.435 1,198.566 196.426,198.566 " />
+        </svg>
+      </div>
       <div className="sub-text">来战个痛快</div>
     </div>
     <div className="list-item-right">
       <Button>打开</Button>
     </div>
-  </div>
+  </div >
 );
 
 const GameListHeader = () => (
