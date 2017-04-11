07e1766c959f1daa2b898470e6b3cd3761bc83f5
woaiso <woaiso@woaiso.com>
2017-04-11 23:15:51 +0800

    add

--- src/index/GameList.tsx
+++ src/index/GameList.tsx
@@ -1,18 +1,19 @@
 import * as React from 'react';
 import Button from './../components/button';
+const exampleIcon = require('./../images/game_01.png');
 /**
  * 游戏列表
  */
 
 const GameItem = () => (
-  <div className="game-list-item">
-    <div className="icon">
-      <img src={""} />
+  <div className="game-list-item with-bottom-line">
+    <div>
+      <img className="game-icon" src={exampleIcon} />
     </div>
     <div className="list-item-content">
       <div className="title">光明大陆</div>
       <div className="group-badge">
-        <Button type="primary" outline={true} className="group-badge">策略类</Button>
+        <Button type="primary" outline={true}>策略类</Button>
       </div>
       <div className="sub-text">来战个痛快</div>
     </div>
@@ -22,10 +23,21 @@ const GameItem = () => (
   </div>
 );
 
+const GameListHeader = () => (
+  <div className="header-wrap">
+    <span className="title">新游尝鲜</span>
+    <a className="more-link" href="javascript:;">更多</a>
+  </div>
+);
+
 export default class GameList extends React.Component<any, any>{
   render() {
     return (
-      <div>
+      <div className="game-list-wrap">
+        <GameListHeader />
+        <GameItem />
+        <GameItem />
+        <GameItem />
         <GameItem />
       </div>
     );
