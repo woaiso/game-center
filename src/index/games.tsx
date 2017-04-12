4904023e9f3f4e6adf3f13d8408db933c22f1d72
woaiso <woaiso@woaiso.com>
2017-04-12 22:44:00 +0800

    add tab

--- src/index/games.tsx
+++ src/index/games.tsx
@@ -3,6 +3,7 @@ import Header from './../components/header';
 import { Page, Content, Slider } from './container';
 import { Game, HotGame } from './main';
 import GameList from './GameList';
+import Tab from './../components/tab';
 import { gamelist, hotGame } from './data';
 /**
  * 游戏界面
@@ -13,7 +14,8 @@ export default class Games extends React.Component<any, any> {
     return (
       <Page>
         <Header />
-        <Content>
+        <Tab hasHeader={true} />
+        <Content withTab={true}>
           <Slider className="game-slider" />
           <Game game={gamelist[0]} />
           <HotGame game={hotGame} />
