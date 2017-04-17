64d2546cd32b9a39cc8d2468499ed2c05ad3a76c
廖云富 <yunful@jumei.com>
2017-04-17 16:52:57 +0800

    add router array

--- src/index/index.tsx
+++ src/index/index.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { renderToStaticMarkup } from 'react-dom/server'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
-import './respones';
+import { renderToStaticMarkup } from 'react-dom/server';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import './../../respones.js';
 import './../components/style/index.less';
 import './main.less';
 
@@ -12,19 +12,46 @@ import Games from './games';
 import Top from './top';
 import Detail from './detail';
 
+const routers = [
+  {
+    path: '/main',
+    component: Main
+  }, {
+    path: '/my-game-temp',
+    component: MyGameTemp
+  }, {
+    path: '/games',
+    component: Games
+  }, {
+    path: '/top',
+    component: Top
+  }, {
+    path: '/detail',
+    component: Detail
+  }
+];
+
 const BasicRouter = () => (
   <Router>
     <div>
-      <Route exact={true} path="/" component={Main} />
-      <Route path="/my-game-temp" component={MyGameTemp} />
-      <Route path="/games" component={Games} />
-      <Route path="/top" component={Top} />
-      <Route path="/detail" component={Detail} />
+      {routers.map((router, index) => <Route key={index} exact={index === 0} path={router.path} component={router.component} />)}
     </div>
   </Router>
 );
 
-//.
-console.log(renderToStaticMarkup(<Main />));
+//renderstatic
+
+let html = [] as any[];
+
+routers.forEach(item => {
+  const Component = item.component;
+  html.push({
+    path: item.path,
+    html: renderToStaticMarkup(<Component />)
+  });
+});
+
+
+console.log(JSON.stringify(html));
 
 ReactDOM.render(<BasicRouter />, document.getElementById('root'));
