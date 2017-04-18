093a972dc66ac4a826e0b87a7a0f7e7a3b24d4cc
woaiso <woaiso@woaiso.com>
2017-04-18 11:38:36 +0800

    replace /images

--- server.js
+++ server.js
@@ -8,9 +8,10 @@ let indexhtml = fs.readFileSync(path.resolve('./dist/index.html'), 'utf-8');
 //去掉 script
 
 indexhtml = indexhtml.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, '');
-indexhtml = indexhtml.replace('</body>', '\n<script>\n'+fs.readFileSync('./respones.js') + '\n</script>\n</body>');
+indexhtml = indexhtml.replace('</body>', '\n<script>\n' + fs.readFileSync('./respones.js') + '\n</script>\n</body>');
 htmls.forEach((html, index) => {
-  const tmp = indexhtml.replace('<div id="root"></div>', html.html);
+  const originHtml = html.html.replace(/\/images\//ig, 'images/');
+  const tmp = indexhtml.replace('<div id="root"></div>', originHtml);
   const prettyData = htmlPretty.prettyPrint(tmp, { indent_size: 2 });
   fs.writeFileSync(path.resolve('./dist/' + html.path + '.html'), prettyData);
 })
