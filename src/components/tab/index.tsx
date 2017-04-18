4956dab7dd5c64a3331b6603df498eebb131789c
woaiso <woaiso@woaiso.com>
2017-04-18 11:39:05 +0800

    add

--- src/components/tab/index.tsx
+++ src/components/tab/index.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
-export default (props) => (
-  <div className={"nav-tab" + (props.hasHeader ? ' has-header' : '')}>
-    <a href="javascript:;" className="tab-item current">
+export default ({ hasHeader, current = 0 }) => (
+  <div className={"nav-tab" + (hasHeader ? ' has-header' : '')}>
+    <a href="javascript:;" className={"tab-item" + (current === 0 ? ' current' : '')}>
       排行榜
     </a>
-    <a href="javascript:;" className="tab-item">
+    <a href="javascript:;" className={"tab-item" + (current === 1 ? ' current' : '')}>
       游戏库
     </a>
   </div>
