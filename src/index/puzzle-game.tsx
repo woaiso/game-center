75af8a40a26baaf502bf4cd21072a4c3431c1b48
woaiso <woaiso@woaiso.com>
2017-04-19 23:28:11 +0800

    edit

--- src/index/puzzle-game.tsx
+++ src/index/puzzle-game.tsx
@@ -8,20 +8,20 @@ const exampleIcon = require('./../images/game_01.png');
 
 import { Star } from './GameList';
 
-const GameItem = ({ loading = false }) => (
+const GameItem = ({ loading = false, hot = false }) => (
   <div className="game-list-item with-bottom-line">
     <div>
       <img className="game-icon" src={exampleIcon} />
     </div>
     <div className="list-item-content">
-      <div className="title">光明大陆</div>
-      <Star />
+      <div className="title">光明大陆 {hot ? <span className="hot-badge" /> : null} </div>
+      <Star className="pull-bottom" />
       <div className="group-badge">
         <Button type="primary" outline={true}>策略类</Button>
       </div>
     </div>
     <div className="list-item-right">
-      {loading ? <Button className="loading-button" /> :<Button className="full-button">打开</Button>}
+      {loading ? <Button className="loading-button" /> : <Button className="full-button">打开</Button>}
     </div>
   </div >
 )
@@ -32,7 +32,7 @@ export default () => (
     <div className="content white">
       <div className="fixed-content" />
       <div className="scroll-content">
-        <GameItem />
+        <GameItem hot={true} />
         <GameItem />
         <GameItem />
         <GameItem />
