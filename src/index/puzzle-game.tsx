9f5e44f93a99af88db46416858c9c4b711cab29e
woaiso <woaiso@woaiso.com>
2017-04-24 00:51:24 +0800

    add

--- src/index/puzzle-game.tsx
+++ src/index/puzzle-game.tsx
@@ -8,20 +8,79 @@ const exampleIcon = require('./../images/game_01.png');
 
 import { Star } from './GameList';
 
-const GameItem = ({ loading = false, hot = false }) => (
+const gamelist = [
+  {
+    icon: require('./../images/game_04.png'),
+    title: '地牢战争',
+    badge: [
+      { button: '策略类' },
+      { button: '3D' }
+    ],
+    subtext: '来战个痛快',
+    hot: true
+  },
+  {
+    icon: require('./../images/game_03.png'),
+    title: '光明大陆',
+    badge: [
+      { button: '策略类' },
+      { button: '3D' }
+    ],
+    subtext: '来战个痛快'
+  },
+  {
+    icon: require('./../images/game_02.png'),
+    title: '糖果苏打传奇',
+    badge: [
+      { button: '策略类' },
+      { button: '3D' }
+    ],
+    subtext: '来战个痛快',
+    loading: true
+  },
+  {
+    icon: require('./../images/game_04.png'),
+    title: '地牢战争',
+    badge: [
+      { button: '策略类' },
+      { button: '3D' }
+    ],
+    subtext: '来战个痛快'
+  },
+  {
+    icon: require('./../images/game_01.png'),
+    title: '我的猫呢',
+    badge: [
+      { button: '策略类' },
+      { button: '3D' }
+    ],
+    subtext: '来战个痛快'
+  },
+  {
+    icon: require('./../images/game_03.png'),
+    title: '冒险村物语',
+    badge: [
+      { button: '策略类' },
+      { button: '3D' }
+    ],
+    subtext: '来战个痛快'
+  }
+]
+
+const GameItem = ({ game = gamelist[0] }) => (
   <div className="game-list-item with-bottom-line">
     <div>
-      <img className="game-icon" src={exampleIcon} />
+      <img className="game-icon" src={game.icon} />
     </div>
     <div className="list-item-content">
-      <div className="title">光明大陆 {hot ? <span className="hot-badge" /> : null} </div>
+      <div className="title">{game.title} {(game as any).hot ? <span className="hot-badge" /> : null} </div>
       <Star className="pull-bottom" />
       <div className="group-badge">
-        <Button type="primary" outline={true}>策略类</Button>
+        {game.badge.map((item, index) => <Button key={index} type="primary" outline={true}>{item.button}</Button>)}
       </div>
     </div>
     <div className="list-item-right">
-      {loading ? <Button className="loading-button" /> : <Button className="full-button">打开</Button>}
+      {(game as any).loading ? <Button className="loading-button" /> : <Button className="full-button">打开</Button>}
     </div>
   </div >
 )
@@ -32,11 +91,7 @@ export default () => (
     <div className="content white">
       <div className="fixed-content" />
       <div className="scroll-content">
-        <GameItem hot={true} />
-        <GameItem />
-        <GameItem />
-        <GameItem />
-        <GameItem loading={true} />
+        {gamelist.map((item, index)=><GameItem key={index} game={item} />)}
       </div>
     </div>
   </div >
