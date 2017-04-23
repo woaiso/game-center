ba54218848a408a4f00be9194e0b758bad1ba515
woaiso <woaiso@woaiso.com>
2017-04-24 00:50:59 +0800

    add data

--- src/index/games-library.tsx
+++ src/index/games-library.tsx
@@ -14,22 +14,29 @@ export default class GamesLibrary extends React.Component<any, any> {
     const tabs = [
       {
         text: '手游',
-        href: 'javascript:;'
+        href: 'javascript:;',
+        active: true
       },
       {
         text: '页游',
         href: 'javascript:;'
       }
     ]
+    const game2 = gamelist[1];
+    game2.title = '动作射击';
+    game2.games = game2.games.reverse();
+
+    const game3 = gamelist[0];
+    game3.title = '跑酷竞速';
     return (
       <Page>
         <Header tabs={tabs} />
         <Tab hasHeader={true} current={1} />
         <Content withTab={true}>
           <Slider className="game-slider" />
-          <Game game={gamelist[ 0 ]} />
-          <Game game={gamelist[ 0 ]} />
-          <Game game={gamelist[ 0 ]} />
+          <Game game={gamelist[0]} />
+          <Game game={game2} />
+          <Game game={game3} />
         </Content>
       </Page>
     );
