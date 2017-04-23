015e86c03afdc9e3f152d43f46ea3408a2532e06
woaiso <woaiso@woaiso.com>
2017-04-24 00:50:53 +0800

    remove star

--- src/index/GameList.tsx
+++ src/index/GameList.tsx
@@ -10,8 +10,9 @@ export class Star extends React.Component<any, any>{
     const { className } = this.props;
     return (
       <div className={"stars" + (className ? ' ' + className : '')}>
-        <span className="star star-temp" />
-        <span className="star star-1" />
+        {/*<span className="star star-temp" />*/}
+
+        {/*<span className="star star-1" />
         <span className="star star-2" />
         <span className="star star-3" />
         <span className="star star-4" />
@@ -19,31 +20,91 @@ export class Star extends React.Component<any, any>{
         <span className="star star-6" />
         <span className="star star-7" />
         <span className="star star-8" />
-        <span className="star star-9" />
+        <span className="star star-9" />*/}
         <span className="star star-full" />
+        <span className="star star-full" />
+        <span className="star star-full" />
+        <span className="star star-full" />
+        <span className="star star-3" />
         <div className="number">8.6</div>
       </div>
     );
   }
 }
 
-export const GameItem = () => (
+const gamelist = [
+  {
+    icon: require('./../images/game_04.png'),
+    title: '地牢战争',
+    badge: [
+      { button: '策略类' },
+      { button: '3D' },
+      { text: '在线玩' }
+    ],
+    subtext: '来战个痛快'
+  },
+  {
+    icon: require('./../images/game_03.png'),
+    title: '光明大陆',
+    badge: [
+      { button: '策略类' },
+      { button: '3D' },
+      { text: '134万人在线玩' }
+    ],
+    subtext: '来战个痛快'
+  },
+  {
+    icon: require('./../images/game_02.png'),
+    title: '糖果苏打传奇',
+    badge: [
+      { button: '策略类' },
+      { button: '3D' },
+      { text: '在线玩' }
+    ],
+    subtext: '来战个痛快'
+  },
+  {
+    icon: require('./../images/game_04.png'),
+    title: '地牢战争',
+    badge: [
+      { button: '策略类' },
+      { button: '3D' },
+      { text: '在线玩' }
+    ],
+    subtext: '来战个痛快'
+  },
+  {
+    icon: require('./../images/game_01.png'),
+    title: '我的猫呢',
+    badge: [
+      { button: '策略类' },
+      { button: '3D' },
+      { text: '134万人在线玩' }
+    ],
+    subtext: '来战个痛快'
+  },
+  {
+    icon: require('./../images/game_03.png'),
+    title: '冒险村物语',
+    badge: [
+      { button: '策略类' },
+      { button: '3D' },
+      { text: '在线玩' }
+    ],
+    subtext: '来战个痛快'
+  }
+]
+
+export const GameItem = ({ game = gamelist[0] }) => (
   <div className="game-list-item with-bottom-line">
     <div>
-      <img className="game-icon" src={exampleIcon} />
+      <img className="game-icon" src={game.icon} />
     </div>
     <div className="list-item-content">
-      <div className="title">光明大陆</div>
+      <div className="title">{game.title}</div>
       <div className="group-badge">
-        <Button type="primary" outline={true}>策略类</Button>
-        <Button type="primary" outline={true}>3D</Button>
-        <span className="badge-text">134万人在线玩</span>
+        {(game as any).badge.map((badge, index) => badge.button ? <Button key={index} type="primary" outline={true}>{badge.button}</Button> : <span key={index} className="badge-text">{badge.text}</span>)}
       </div>
-      {/*<div className="star">
-        <Star />
-        <Star />
-        <Star />
-      </div>*/}
       <div className="sub-text">来战个痛快</div>
     </div>
     <div className="list-item-right">
@@ -64,10 +125,7 @@ export default class GameList extends React.Component<any, any>{
     return (
       <div className="game-list-wrap">
         <GameListHeader />
-        <GameItem />
-        <GameItem />
-        <GameItem />
-        <GameItem />
+        {gamelist.map((game, index) => <GameItem key={index} game={game} />)}
       </div>
     );
   }
